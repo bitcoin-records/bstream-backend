a94077ee1e64a51cdb9cb72601e858dedf7a0aed
Soravis Prakkamakul <asunnotthesun@gmail.com>
2017-03-26 12:56:23 -0700

    Change request agent to superagent, reformat request

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const nodeRouter = require('./nodeRouter.js');
 const cookieParser = require('cookie-parser');
-const request = require('request');
+const request = require('superagent');
 var cors = require('cors')
 
 const app = express();
 app.set('port', (process.env.PORT || 5000));
-const BCOIN_REST_API = 'http://localhost:5000/node';
+const BCOIN_REST_API = 'http://127.0.0.1:18556';
 
 var userDb = {
   Billy: {
@@ -20,6 +20,10 @@ var userDb = {
 var artistsToPay = {
   NOFX: {
     artistReceiveAddress: 'SVS5LCYh3XortwyqKdw8Bnd1dzBRrP3Sdk',
+    btcEarned: 0,
+  },
+  artist1: {
+    artistReceiveAddress: 'SRGZ8fto4yW8FXE2wVzR93nf55szP9FvM1',
     btcEarned: 0,
   },
 };
@@ -75,22 +79,37 @@ app.post('/track-stream', (req, res) => {
     // Add btc ammount to artist btcEarned
     artistsToPay[data.artist].btcEarned += data.trackPrice;
 
+    const url = `${BCOIN_REST_API}/wallet/${user.userWalletId}/send`;
     request
-      .post(`${BCOIN_REST_API}/wallet/${user.userWalletId}/send`)
-      .form({
-        rate: '0.001',
-        outputs: [{ value: '0.1', address: artist.artistReceiveAddress }],
-      })
-      .on('response', function(response) {
-        res.status(200).json({
-          balance: userDb[data.username].balance,
-          artist: artist
-        });
-      })
-      .on('error', function(err) {
-        res.status(400).json(err);
-        console.log(err)
-      })
+      .post(url)
+      .send({
+          rate: 0.0,
+          outputs: [
+            {
+              value: ("" + data.trackPrice), 
+              address: artist.artistReceiveAddress 
+            }
+          ]
+        })
+      .set('Accept', 'application/json')
+      .end(function(err, response){
+        if (err) {
+          console.error(err);
+          res.status(400).json({
+            success: false,
+            bcoinResponse: JSON.parse(response.text)
+          });
+        } else {
+          console.log(response.statusCode);
+          console.log(response);
+          res.status(200).json({
+            success: true,
+            balance: userDb[data.username].balance,
+            artist: artist,
+            bcoinResponse: JSON.parse(response.text)
+          });
+        }
+      });
   }
 });
 
